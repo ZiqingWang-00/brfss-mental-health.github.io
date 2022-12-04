b6c1a3d9875cc9cc8cae9c6c691a17418e722ff6
ZiqingWang-00 <zw2899@cumc.columbia.edu>
2022-12-03 23:20:17 -0500

    changed mh_bin variable, computed AIC, started some testing code for potential shiny app

---
+++ Prediction.R
@@ -0,0 +1,15 @@
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+
+# Define UI for app 
+ui <- fluidPage()
+    
+# Define server logic
+server <- function(input, output) { }
+
+# Create Shiny app ----
+shinyApp(ui = ui, server = server)
+
+
+
